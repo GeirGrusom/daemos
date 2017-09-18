9dbf669feb1ba7222593282d4e00e1454d38509b
Henning Moe <henning.moe@hotmail.com>
2017-09-18 22:45:58 +0200

    Added more tests for AutoResetEvent.

--- tests/Tests/Threading/AutoResetEventTests.cs
+++ tests/Tests/Threading/AutoResetEventTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -11,23 +9,26 @@ namespace Daemos.Tests.Threading
 {
     public class AutoResetEventTests
     {
+
+        private static void Timeout(Action @event, int ms)
+        {
+            var timer = new Timer(o => @event(), null, ms, System.Threading.Timeout.Infinite);
+        }
+
         [Fact]
         public async Task WaitOne_ReleasesOnce_ReturnsTrue()
         {
             var ev = new AutoResetEvent(false);
-            var thread = new Thread(() =>
-            {
-                Thread.Sleep(100);
-                ev.Set();
-            });
+            
 
             var stp = Stopwatch.StartNew();
-            thread.Start();
+            Timeout(() => ev.Set(), 10);
+
             var result = await ev.WaitOne();
             stp.Stop();
 
             Assert.True(result);
-            Assert.True(stp.ElapsedMilliseconds >= 100);
+            Assert.True(stp.ElapsedMilliseconds >= 10);
         }
 
         [Fact]
@@ -68,11 +69,11 @@ namespace Daemos.Tests.Threading
             var ev = new AutoResetEvent(false);
 
             var stp = Stopwatch.StartNew();
-            var cancel = new CancellationTokenSource(100);
+            var cancel = new CancellationTokenSource(10);
             await Assert.ThrowsAsync<TaskCanceledException>( () => ev.WaitOne(cancel.Token));
             stp.Stop();
 
-            Assert.True(stp.ElapsedMilliseconds >= 100);
+            Assert.True(stp.ElapsedMilliseconds >= 10);
         }
 
         [Fact]
@@ -81,11 +82,78 @@ namespace Daemos.Tests.Threading
             var ev = new AutoResetEvent(false);
 
             var stp = Stopwatch.StartNew();
-            var result = await ev.WaitOne(100);
+            var result = await ev.WaitOne(10);
             stp.Stop();
 
             Assert.False(result);
-            Assert.True(stp.ElapsedMilliseconds >= 100);
+            Assert.True(stp.ElapsedMilliseconds >= 10);
+        }
+
+        // Tests that Dispose does not throw when task has timed out
+        [Fact]
+        public async Task WaitOne_Timeout_Dispose()
+        {
+            var ev = new AutoResetEvent(false);
+
+            var result = await ev.WaitOne(10);
+
+            Assert.False(result);
+            ev.Dispose();
+        }
+
+        // Tests that Dispose does not throw when task has been cancelled
+        [Fact]
+        public async Task WaitOne_Cancel_Dispose()
+        {
+            var ev = new AutoResetEvent(false);
+            var cancel = new CancellationTokenSource(10);
+
+            try
+            {
+                await ev.WaitOne(cancel.Token);
+                throw new InvalidOperationException("WaitOne should throw a TaskCancelledException");
+            }
+            catch(TaskCanceledException)
+            {
+            }
+
+            ev.Dispose();
+        }
+
+        // Checks that Dispose works even when both cancellation and timeout occurred
+        [Fact]
+        public async Task WaitOne_CancelAndTimeout_Dispose()
+        {
+            var ev = new AutoResetEvent(false);
+            var cancel = new CancellationTokenSource(25);
+
+            var result = await ev.WaitOne(10, cancel.Token);
+
+            await Task.Delay(25);
+
+            Assert.False(result);
+            ev.Dispose();
+        }
+
+        // Checks that Dispose works even when both timeout and cancellation occurred
+        [Fact]
+        public async Task WaitOne_TimeoutAndCancel_Dispose()
+        {
+            var ev = new AutoResetEvent(false);
+            var cancel = new CancellationTokenSource(10);
+
+            try
+            { 
+                await ev.WaitOne(25, cancel.Token);
+                throw new InvalidOperationException("WaitOne should throw a TaskCancelledException");
+            }
+            catch (TaskCanceledException)
+            {
+            }
+
+            await Task.Delay(50);
+
+            ev.Dispose();
         }
     }
 }
